a26d098ae770171f938b856e2f6b50b2970be34d
cday97 <christophersday@gmail.com>
2022-06-23 13:16:22 -0600

    Add college, nursing, and healthcare data to efa analysis

--- R/efamap/efa_analysis_2020.R
+++ R/efamap/efa_analysis_2020.R
@@ -10,12 +10,14 @@
 #'@param minority Table B03002 from ACS data, organized by block groups in the region
 #'@param vehicles Table B25044 from ACS data, organized by block groups in the region
 #'@param income Table C17002 from ACS data, organized by block groups in region
+#'@param groupQuarter Table P5 from ACS data, organized by block groups in region
 #'
 #' Old EFAs: https://data.wfrc.org/datasets/equity-focus-areas/explore?location=40.772488%2C-111.854934%2C9.71&showTable=true
 #' Utah Block Groups 2020: https://opendata.gis.utah.gov/datasets/utah-census-block-groups-2020/explore?location=39.472883%2C-111.547240%2C-1.00
 #' Minority Table: https://data.census.gov/cedsci/table?q=B03002&g=0500000US49003%241500000,49011%241500000,49035%241500000,49049%241500000,49057%241500000&y=2020
 #' Vehicle Table: https://data.census.gov/cedsci/table?q=B25044&g=0500000US49003%241500000,49011%241500000,49035%241500000,49049%241500000,49057%241500000&y=2020
 #' Income Table: https://data.census.gov/cedsci/table?q=C17002&g=0500000US49003%241500000,49011%241500000,49035%241500000,49049%241500000,49057%241500000&y=2020
+#' Group Quarter Table: https://data.census.gov/cedsci/table?q=Group%20Quarter&g=0500000US49003%241500000,49011%241500000,49035%241500000,49049%241500000,49057%241500000&y=2020&tid=DECENNIALPL2020.P5
 #'
 #'@return Two new shapefiles containing the updated EFAs calculated from 2020 data. 
 #'One shapefile is created using standard devations from the mean and the other is 
@@ -47,13 +49,14 @@ utahblocksshp <- st_read("data/Utah_Census_Block_Groups_2020/CensusBlockGroups20
 minority <- read_csv("data/ACSDT5Y2020.B03002/ACSDT5Y2020.B03002_data_with_overlays_2022-06-07T113506.csv")
 vehicles <- read_csv("data/ACSDT5Y2020.B25044/ACSDT5Y2020.B25044_data_with_overlays_2022-06-07T123124.csv")
 income <- read_csv("data/ACSDT5Y2020.C17002/ACSDT5Y2020.C17002_data_with_overlays_2022-06-07T123423.csv")
+groupQuarter <- read_csv("data/DECENNIALPL2020.P5/DECENNIALPL2020.P5_data_with_overlays_2022-06-23T135404.csv")
 
 #functions -------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 delete_low_pop_dens <- function(table){
   table %>% 
     mutate(Area_Meters = as.numeric(st_area(SHAPE))) %>%
     mutate(Area_Miles = Area_Meters / 2589988.11) %>%
-    mutate(PopDens = Population / Area_Miles) %>%
+    mutate(PopDens = round(Population / Area_Miles),5) %>%
     filter(PopDens > 2000)
 }
 
@@ -83,10 +86,16 @@ PovertyTable2020 <- income[-1,] %>%
   select(NAME,GEO_ID,Poverty,PercPovert) %>%
   mutate(Perc_Pov25 = ifelse(PercPovert > 0.25,1,0),
          Perc_Pov20 = ifelse(PercPovert > 0.2,1,0))
+GroupQuarter2020 <- groupQuarter [-1,] %>%
+  rename("IP" = P5_002N, "IP_Correctional" = P5_003N,"IP_Juvenile" = P5_004N, "IP_Nursing" = P5_005N, "IP_Other" = P5_006N,
+         "NIP" = P5_007N, "NIP_College"=P5_008N,"NIP_Military"=P5_009N,"NIP_Other"=P5_010N) %>%
+  mutate(IP = as.numeric(IP),IP_Correctional=as.numeric(IP_Correctional),IP_Juvenile=as.numeric(IP_Juvenile),IP_Nursing=as.numeric(IP_Nursing),IP_Other=as.numeric(IP_Other),
+         NIP = as.numeric(NIP),NIP_College=as.numeric(NIP_College),NIP_Military=as.numeric(NIP_Military),NIP_Other=as.numeric(NIP_Other))
 
 #' join together all three tables
 efa2020 <- left_join(MinorityTable2020,VehicleTable2020,by= c("NAME","GEO_ID")) %>%
   left_join(PovertyTable2020,by=c("NAME","GEO_ID")) %>%
+  left_join(GroupQuarter2020, by = c("NAME","GEO_ID")) %>%
   mutate(HighestPerc25wCar = pmax(Perc_Pov25,Perc_ZeroCar,Perc_Minorit),
          HighestPerc20wCar = pmax(Perc_Pov20,Perc_ZeroCar,Perc_Minorit),
          HighestPerc25woCar = pmax(Perc_Pov25,Perc_Minorit),
@@ -120,7 +129,7 @@ efa2020shpb4 <- left_join(efa2020,utahblocksshp,by="GEOID20") %>%
   mutate(OBJECTID = row_number(), SHAPE=geometry)
 # select the columns needed for further analysis
 efa2020shp <- efa2020shpb4 %>%
-  select(OBJECTID,SHAPE,Geography,Population,Poverty,PercPovert,SD_Pov,Perc_Pov25,Perc_Pov20,Minority,PercMinori,SD_Minorit,Perc_Minorit,ZeroCar,PercZeroCa,SD_ZeroCar,Perc_ZeroCar,HighestStDev,HighestPerc25wCar,HighestPerc20wCar,HighestPerc25woCar,HighestPerc20woCar,SHAPE_Leng,SHAPE_Area)
+  select(OBJECTID,SHAPE,Geography,Population,Poverty,PercPovert,SD_Pov,Perc_Pov25,Perc_Pov20,Minority,PercMinori,SD_Minorit,Perc_Minorit,ZeroCar,PercZeroCa,SD_ZeroCar,Perc_ZeroCar,HighestStDev,HighestPerc25wCar,HighestPerc20wCar,HighestPerc25woCar,HighestPerc20woCar,SHAPE_Leng,SHAPE_Area,IP,IP_Correctional,IP_Juvenile,IP_Nursing,IP_Other,NIP,NIP_College,NIP_Military,NIP_Other)
 
 # Create Threshold Comparison Table -------------------------------------------------------------------------------------------------------------------------------------------------------------#
 #' create a table comparing the default thresholds and the calculated thresholds
@@ -182,13 +191,57 @@ efaAnalysis <- bind_rows(list(efaPerc2020Pov25NoCarshp,efaPerc2020Pov20NoCarshp,
 #st_write(efaPerc2017Pov20NoCarshp, dsn = "outputs/results/EFAs_2017_2.gpkg", layer = "EFA2017Pov20NoCar",append=TRUE)
 
 
+# Extra data to view in Maps --------------------------------------------------------------------------------------------------------------------------------------------------------#
+#' @param utahHealth A shapefile showing all the health care facilities in Utah
+#' @param utahSchools A shapefile showing all the higher education facilities in Utah
+#'
+#' Health Care Facilities: https://opendata.gis.utah.gov/datasets/f5e5d7c717c946fe9cd6f2c8d0bf8d86_0/explore
+#' Higher Education Facilities: https://opendata.gis.utah.gov/datasets/utah-schools-higher-education/explore
+
+utahHealth <- st_read("data/Utah_Health_Care_Facilities/HealthCareFacilities.shp") %>%  st_transform(4326) %>%
+  mutate(intersection = as.integer(st_intersects(geometry, wfrcboundary))) %>% filter(is.na(intersection) == FALSE) %>%
+  mutate(group = "Health Care Facilities")
+utahSchools <- st_read("data/Utah_Schools_Higher_Education/Schools_HigherEducation.shp") %>%  st_transform(4326) %>%
+  mutate(intersection = as.integer(st_intersects(geometry, wfrcboundary))) %>% filter(is.na(intersection) == FALSE) %>%
+  mutate(group = "Higher Education Facilities")
+
+
 # View in Maps-------------------------------------------------------------------------------------------------------------------------------------------------------------#
 plot_comparison_map <- function(){
-  leaflet(efaAnalysis) %>%
-  addPolygons(data = efaAnalysis$SHAPE, fillOpacity = .5, fillColor = efaAnalysis$color, color = efaAnalysis$color, group = efaAnalysis$group, weight = .8) %>%
+  leaflet() %>%
+  addPolygons(data = efaAnalysis$SHAPE, 
+              fillOpacity = .5, 
+              fillColor = efaAnalysis$color, 
+              color = efaAnalysis$color, 
+              group = efaAnalysis$group, 
+              weight = .8,
+              popup = paste("Population:", efaAnalysis$Population, "<br>",
+                            "Population Density:", efaAnalysis$PopDens, "<br>",
+                            "Poverty %:", efaAnalysis$PercPovert, "<br>",
+                            "Minority %:", efaAnalysis$PercMinori, "<br>",
+                            "ZeroCar %:", efaAnalysis$PercZeroCa, "<br>",
+                            "Institutionalized Population:", efaAnalysis$IP, "<br>",
+                            "Nursing/Skilled-Nursing Facilities:", efaAnalysis$IP_Nursing, "<br>",
+                            "Noninstitutionalized Population:", efaAnalysis$NIP, "<br>",
+                            "College/University Student Housing:", efaAnalysis$NIP_College))%>%
   addProviderTiles(providers$CartoDB.Positron) %>%  # providers$Esri.WorldStreetMap
+  addCircleMarkers(data = utahHealth, 
+                   radius = 2, 
+                   color = "orange",
+                   group = utahHealth$group,
+                   popup = paste("Name:",utahHealth$NAME, "<br>",
+                                 "Type:",utahHealth$TYPE, "<br>",
+                                 "City:",utahHealth$CITY)) %>%
+  addCircleMarkers(data = utahSchools, 
+                   radius = 2, 
+                   color = "black", 
+                   group = utahSchools$group,
+                   popup = paste("Name:", utahSchools$SchoolName, "<br>",
+                                 "Type:", utahSchools$SchoolType, "<br>",
+                                 "Campus:", utahSchools$CampusType, "<br>",
+                                 "City:", utahSchools$City)) %>%
   addLayersControl(
-    overlayGroups = efaAnalysis$group,
+    overlayGroups = c("2020 Pov-25% Min-40%","2020 Pov-20% Min-40%","2020 Pov-20% Min 40% Veh-10%","2017 Pov-25% Min-40%","2017 Pov-20% Min-40%",utahHealth$group,utahSchools$group),
     options = layersControlOptions(collapsed=FALSE)
   )
 }
