8c20f871fe896fc386da6c4eec07e641367a5ca4
cday97 <christophersday@gmail.com>
2022-06-23 11:00:36 -0600

    Create shiny application to display map
    Instead of viewing the EFA secondary analysis in ArcGis, these changes create a website comparing the differences in different EFA datasets.

---
+++ R/efamap/app.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(sf)
+library(knitr)
+library(kableExtra)
+library(magrittr)
+library(leaflet)
+source("efa_analysis_2020.R")
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+    leafletOutput("map")
+  )
+)
+
+server <- function(input, output) {
+  output$map <- renderLeaflet({
+    plot_comparison_map() %>% setView(lng = -111.8910, lat = 40.7608, zoom = 9)
+  })
+}
+
+
+shinyApp(ui = ui, server = server)
+
