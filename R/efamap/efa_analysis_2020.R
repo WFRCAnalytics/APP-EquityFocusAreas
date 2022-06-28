68d2b4ac7d49b06324e334a9df42b4e7bd49e4bb
cday97 <christophersday@gmail.com>
2022-06-28 11:14:44 -0600

    Clean code and update script to create histograms and

--- R/efamap/efa_analysis_2020.R
+++ R/efamap/efa_analysis_2020.R
@@ -36,25 +36,10 @@ library(magrittr)
 library(leaflet)
 library(tidycensus)
 library(tigris)
+source("efa_analysis_scripts.R")
+#source("R/efamap/efa_analysis_scripts.R")
 census_api_key("0196454888e2441971be7360589dd0399e036978")
 
-#functions -------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-delete_low_pop_dens <- function(table){
-  table %>% 
-    mutate(Area_Meters = as.numeric(st_area(SHAPE))) %>%
-    mutate(Area_Miles = Area_Meters / 2589988.11) %>%
-    mutate(PopDens = round(Population / Area_Miles),5) %>%
-    filter(PopDens > 2000)
-}
-
-get_acs_wfrc <- function(geo,vars,state,counties,year){
-  get_acs(geography = geo,
-          variables = vars,
-          state = state,
-          county = counties,
-          year = year
-  )
-}
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Read in the data needed to redetermine the EFAs
@@ -70,45 +55,27 @@ wfrc_blockgroups <- block_groups("UT",county = wfrc_counties,cb=TRUE) %>%
   st_transform(4326)
 
 minVars <- paste0("B03002_00", c(1,3))
-minority <- get_acs_wfrc("block group", minVars, "UT", wfrc_counties,2020) %>% select(-moe) %>%
+minority20 <- get_acs_wfrc("block group", minVars, "UT", wfrc_counties,2020) %>% select(-moe) %>%
   pivot_wider(names_from = variable,values_from = c(estimate))
   
 vehVars <- c(paste0("B25044_00",c(1,3)),"B25044_010")
-vehicles <- get_acs_wfrc("block group", vehVars, "UT", wfrc_counties, 2020) %>% select(-moe) %>%
+vehicles20 <- get_acs_wfrc("block group", vehVars, "UT", wfrc_counties, 2020) %>% select(-moe) %>%
   pivot_wider(names_from = variable,values_from = c(estimate))
 
 incVars <- paste0("C17002_00", c(1:3))
-income <- get_acs_wfrc("block group",incVars,"UT",wfrc_counties,2020) %>% select(-moe) %>%
+income20 <- get_acs_wfrc("block group",incVars,"UT",wfrc_counties,2020) %>% select(-moe) %>%
   pivot_wider(names_from = variable,values_from = c(estimate))
 
 groupQuarter <- read_csv("data/DECENNIALPL2020.P5/DECENNIALPL2020.P5_data_with_overlays_2022-06-23T135404.csv")
-#groupVars <- c(paste0("P5_00",c(2:9)),"P5_010")
-#groupQuarter <- get_acs_wfrc("block group",groupVars,"UT",2020) %>% select(-moe) %>%
-#  pivot_wider(names_from = variable,values_from = c(estimate))
+
 
 # Basic Table Analysis -------------------------------------------------------------------------------------------------------------------------------------------------------------#
 #' manipulate all three tables used to determine EFAs. For each table, calculate the percentage
 #' for each block group. Additionally, calculate whether or not each block meets the default
 #' threshold set a few years ago
-MinorityTable2020 <- minority %>%
-  rename("Population"=B03002_001, "White_E"=B03002_003) %>%
-  mutate(Minority = Population - White_E) %>%
-  mutate("PercMinori" = round(Minority/Population,3)) %>%
-  select(NAME,GEOID,Population,Minority,PercMinori) %>%
-  mutate(Perc_Minorit = ifelse(PercMinori > 0.4, 1,0))
-VehicleTable2020 <- vehicles %>%
-  rename("TotalHH"=B25044_001, "OwnerZeroVehs_E"=B25044_003, "RenterZeroVehs_E" = B25044_010) %>%
-  mutate(ZeroCar = OwnerZeroVehs_E + RenterZeroVehs_E) %>%
-  mutate(PercZeroCa = round(ZeroCar/TotalHH,3)) %>%
-  select(NAME,GEOID,ZeroCar,PercZeroCa) %>%
-  mutate(Perc_ZeroCar = ifelse(PercZeroCa > 0.1,1,0))
-PovertyTable2020 <- income %>%
-  rename("pop" = C17002_001, "superpoor_e" = C17002_002, "poor_e" = C17002_003) %>%
-  mutate(Poverty = superpoor_e + poor_e) %>%
-  mutate(PercPovert = round(Poverty/pop,3)) %>%
-  select(NAME,GEOID,Poverty,PercPovert) %>%
-  mutate(Perc_Pov25 = ifelse(PercPovert > 0.25,1,0),
-         Perc_Pov20 = ifelse(PercPovert > 0.2,1,0))
+MinorityTable2020 <- minority_percent(minority20)
+PovertyTable2020 <- poverty_percent(income20)
+VehicleTable2020 <- vehicle_percent(vehicles20)
 
 # Extra data to display on the final map
 GroupQuarter2020 <- groupQuarter [-1,] %>%
@@ -119,13 +86,7 @@ GroupQuarter2020 <- groupQuarter [-1,] %>%
          GEOID = substring(GEO_ID,10)) %>% select(-GEO_ID)
 
 #' join together all four tables
-efa2020 <- left_join(MinorityTable2020,VehicleTable2020,by= c("NAME","GEOID")) %>%
-  left_join(PovertyTable2020,by=c("NAME","GEOID")) %>%
-  left_join(GroupQuarter2020, by = c("NAME","GEOID")) %>%
-  mutate(HighestPerc25wCar = pmax(Perc_Pov25,Perc_ZeroCar,Perc_Minorit),
-         HighestPerc20wCar = pmax(Perc_Pov20,Perc_ZeroCar,Perc_Minorit),
-         HighestPerc25woCar = pmax(Perc_Pov25,Perc_Minorit),
-         HighestPerc20woCar = pmax(Perc_Pov20,Perc_Minorit))
+efa2020 <- initial_join20(MinorityTable2020,VehicleTable2020,PovertyTable2020,GroupQuarter2020)
 
 
 # Join Tables and Region Geography -------------------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -134,48 +95,11 @@ efa2020 <- left_join(MinorityTable2020,VehicleTable2020,by= c("NAME","GEOID")) %
 #' are not within the WFRC/MAG model region. Also, determine whether or not each 
 #' region's percentage is within the calculated thresehold (whether or not it is 
 #' at least one standard devation from the region's mean)
-efa2020shpb4 <- left_join(efa2020,wfrc_blockgroups,by="GEOID") %>%
-  mutate("Geography"=NAME.x) %>%
-  mutate(within = as.character(st_within(geometry,wfrcboundary$geometry))) %>%
-  filter(within == "1",is.na(PercZeroCa)==FALSE) %>%
-  mutate(meanMin = mean(PercMinori),
-         sdMin = sd(PercMinori),
-         meanVeh = mean(PercZeroCa),
-         sdVeh = sd(PercZeroCa),
-         meanPov = mean(PercPovert),
-         sdPov = sd(PercPovert)) %>%
-  mutate(povsd = (PercPovert - meanPov)/sdPov,
-         minsd = (PercMinori - meanMin)/sdMin,
-         vehsd = (PercZeroCa - meanVeh)/sdVeh) %>%
-  mutate(SD_Pov = ifelse(povsd >= 2, 2, ifelse(povsd >= 1, 1, 0)),
-         SD_ZeroCar = ifelse(vehsd >= 2, 2, ifelse(vehsd >= 1, 1, 0)),
-         SD_Minorit = ifelse(minsd >= 2, 2, ifelse(minsd >= 1, 1, 0))) %>%
-  mutate(HighestStDev = pmax(SD_Pov,SD_ZeroCar,SD_Minorit)) %>%
-  mutate(OBJECTID = row_number(), SHAPE=geometry)
+efa2020shpb4 <- geometry_calculate(efa2020,wfrc_blockgroups)
+
 # select the columns needed for further analysis
 efa2020shp <- efa2020shpb4 %>%
   select(OBJECTID,SHAPE,Geography,Population,Poverty,PercPovert,SD_Pov,Perc_Pov25,Perc_Pov20,Minority,PercMinori,SD_Minorit,Perc_Minorit,ZeroCar,PercZeroCa,SD_ZeroCar,Perc_ZeroCar,HighestStDev,HighestPerc25wCar,HighestPerc20wCar,HighestPerc25woCar,HighestPerc20woCar,IP,IP_Correctional,IP_Juvenile,IP_Nursing,IP_Other,NIP,NIP_College,NIP_Military,NIP_Other)
-
-# Create Threshold Comparison Table -------------------------------------------------------------------------------------------------------------------------------------------------------------#
-#' create a table comparing the default thresholds and the calculated thresholds
-#' based on this 2020 data
-efatestd<- efa2020shpb4 %>%
-  summarize(calculatedPovPerc = round(sdPov+meanPov,3),
-            assumedPovPerc = 0.250,
-            calculatedMinPerc = round(sdMin+meanMin,3),
-            assumedMinPerc = 0.400,
-            calculatedVehPerc = round(sdVeh+meanVeh,3),
-            assumedVehPerc = 0.100) %>%
-  unique() %>%
-  pivot_longer(everything(),values_to = "values", names_to = "headers") %>%
-  mutate(Type = ifelse(grepl("calc",headers),"Calculated","Assumed"),
-         Category = ifelse(grepl("Pov",headers),"Poverty",ifelse(grepl("Min",headers),"Minority","ZeroCar"))) %>%
-  select(-headers)%>%
-  pivot_wider(names_from=Category,values_from = c(values))
-
-efatestd %>% 
-  kbl(caption = "Equity Focus Area Thresholds") %>%
-  kable_classic(full_width = F, html_font = "Cambria")
 
 
 #Create EFA GeoPackage for Original Analysis -------------------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -204,8 +128,9 @@ efaPerc2017shp <- efashp2017 %>%  mutate(Perc_Pov25 = ifelse(PercPovert > 0.25,1
   mutate(HighestPerc25woCar = pmax(Perc_Pov25,SD_Minorit), HighestPerc20woCar = pmax(Perc_Pov20,SD_Minorit),SHAPE = geometry)
 efaPerc2017Pov25NoCarshp <- efaPerc2017shp %>% filter(HighestPerc25woCar > 0) %>% mutate(group = "2017 Pov-25% Min-40%",color = "green")
 efaPerc2017Pov20NoCarshp<- efaPerc2017shp %>% filter(HighestPerc20woCar > 0) %>% mutate(group = "2017 Pov-20% Min-40%",color = "purple")
+efaPerc2017Pov25Carshp <- efaPerc2017shp %>% filter(HighestStD > 0) %>% mutate(group = "2017 Pov-25% Min-40% Veh-10%", color = "gray")
 
-efaAnalysis <- bind_rows(list(efaPerc2020Pov25NoCarshp,efaPerc2020Pov20NoCarshp,efaPerc2020Pov20Carshp,efaPerc2017Pov25NoCarshp,efaPerc2017Pov20NoCarshp)) %>%
+efaAnalysis <- bind_rows(list(efaPerc2020Pov25NoCarshp,efaPerc2020Pov20NoCarshp,efaPerc2020Pov20Carshp,efaPerc2017Pov25NoCarshp,efaPerc2017Pov20NoCarshp,efaPerc2017Pov25Carshp)) %>%
   mutate(group = as.factor(group))
 
 # TODO: Would geojson be better than geopackage? Look into this.
@@ -216,7 +141,7 @@ efaAnalysis <- bind_rows(list(efaPerc2020Pov25NoCarshp,efaPerc2020Pov20NoCarshp,
 #st_write(efaPerc2017Pov20NoCarshp, dsn = "outputs/results/EFAs_2017_2.gpkg", layer = "EFA2017Pov20NoCar",append=TRUE)
 
 
-# Extra data to view in Maps --------------------------------------------------------------------------------------------------------------------------------------------------------#
+# MAP ANALYSIS --------------------------------------------------------------------------------------------------------------------------------------------------------#
 #' @param utahHealth A shapefile showing all the health care facilities in Utah
 #' @param utahSchools A shapefile showing all the higher education facilities in Utah
 #'
@@ -231,7 +156,6 @@ utahSchools <- st_read("data/Utah_Schools_Higher_Education/Schools_HigherEducati
   mutate(group = "Higher Education Facilities")
 
 
-# View in Maps-------------------------------------------------------------------------------------------------------------------------------------------------------------#
 plot_comparison_map <- function(){
   leaflet() %>%
   addPolygons(data = efaAnalysis$SHAPE, 
@@ -266,7 +190,36 @@ plot_comparison_map <- function(){
                                  "Campus:", utahSchools$CampusType, "<br>",
                                  "City:", utahSchools$City)) %>%
   addLayersControl(
-    overlayGroups = c("2020 Pov-25% Min-40%","2020 Pov-20% Min-40%","2020 Pov-20% Min 40% Veh-10%","2017 Pov-25% Min-40%","2017 Pov-20% Min-40%",utahHealth$group,utahSchools$group),
+    overlayGroups = c("2020 Pov-25% Min-40%","2020 Pov-20% Min-40%","2020 Pov-20% Min 40% Veh-10%","2017 Pov-25% Min-40%","2017 Pov-20% Min-40%","2017 Pov-25% Min-40% Veh-10%",utahHealth$group,utahSchools$group),
     options = layersControlOptions(collapsed=FALSE)
   )
 }
+
+
+# TABLE ANALYSIS -------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#' create a table comparing the default thresholds and the calculated thresholds
+#' based on this 2020 data
+efatestd<- percentage_table(efa2020shpb4)
+
+efatestd %>% 
+  kbl(caption = "Equity Focus Area Thresholds") %>%
+  kable_classic(full_width = F, html_font = "Cambria")
+
+
+# HISTOGRAM ANALYSIS -------------------------------------------------------------------------------------------------------------------------------------------------------------#
+efaHisto20 <- efa2020shpb4 %>%
+  mutate(calcPov = round(sdPov+meanPov,3), assumePov = 0.25,
+         calcMin = round(sdMin+meanMin,3), assumeMin = 0.40,
+         calcVeh = round(sdVeh+meanVeh,3), assumeVeh = 0.10) %>%
+  select(OBJECTID,Geography,Population,Poverty,PercPovert,meanPov,sdPov,calcPov,assumePov,Minority,PercMinori,meanMin,sdMin,calcMin,assumeMin,ZeroCar,PercZeroCa,meanVeh,sdVeh,calcVeh,assumeVeh,SHAPE) %>%
+  pivot_longer(c(Poverty,Minority,ZeroCar), names_to = "Variable", values_to = "Total") %>%
+  mutate(Percentage = ifelse(grepl("Pov",Variable), PercPovert, ifelse(grepl("Min",Variable), PercMinori,PercZeroCa)),
+         Calculated = ifelse(grepl("Pov",Variable),calcPov,ifelse(grepl("Min",Variable),calcMin,calcVeh)),
+         Assumed = ifelse(grepl("Pov",Variable),assumePov,ifelse(grepl("Min",Variable),assumeMin,assumeVeh)),
+         Year = 2020) %>%
+  select(OBJECTID,Geography,Year,Population,Variable,Total,Percentage,Calculated,Assumed)
+
+#createHistogram(efaHisto20,"ZeroCar", 2020)
+#createHistogram(efaHisto20, "Minority", 2020)
+#reateHistogram(efaHisto20, "Poverty", 2020)
+
